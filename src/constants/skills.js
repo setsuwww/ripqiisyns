18740b84ca00913d04d7f9649824e3b524f4c0c9
unknown <setsumsc@gmail.com>
2025-07-27 17:03:08 +0700

    Nama Skill yang disingkat

--- src/constants/skills.js
+++ src/constants/skills.js
@@ -1,8 +1,8 @@
 export const skills = [
-  { name: "Next.js", level: 95, color: "from-white to-gray-400" },
-  { name: "React.js", level: 95, color: "from-sky-400 to-sky-600" },
-  { name: "Nuxt.js", level: 90, color: "from-emerald-400 to-emerald-600" },
-  { name: "Vue.js", level: 90, color: "from-green-400 to-green-600" },
+  { name: "Nextjs", level: 95, color: "from-white to-gray-400" },
+  { name: "Reactjs", level: 95, color: "from-sky-400 to-sky-600" },
+  { name: "Nuxtjs", level: 90, color: "from-emerald-400 to-emerald-600" },
+  { name: "Vuejs", level: 90, color: "from-green-400 to-green-600" },
   { name: "Laravel", level: 95, color: "from-red-400 to-red-600" },
   { name: "PHP", level: 80, color: "from-purple-400 to-purple-600" },
   { name: "TypeScript", nameSingkat: "TS", level: 85, color: "from-blue-400 to-blue-500" },

70e70f53a44616dcc573e76c97c1071661328af8
unknown <setsumsc@gmail.com>
2025-08-09 23:08:43 +0700

    ganti image project

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -6,8 +6,6 @@ export const projects = [
       "This is my Simple Content Management System Web app, was made with Vuejs & Vite for high SPA & PWA Performance.",
     image: "/vuetyfull.webp",
     technologies: ["MySQL", "Expressjs", "Prisma", "Vuejs", "Vite", "Tailwindcss"],
-    github: "#",
-    demo: "#",
   },
   {
     id: 2,
@@ -16,8 +14,6 @@ export const projects = [
     "This is my first Nextjs Todolist app for Client Project & Request, i was use Nextjs for Performance and smooth DX.",
     image: "/Nexttask.webp",
     technologies: ["MySQL", "Prisma", "Nextjs", "React", "Typescript", "Tailwindcss"],
-    github: "#",
-    demo: "#",
   },
   {
     id: 3,
@@ -25,9 +21,7 @@ export const projects = [
     description:
       "This is a My Biggest Project for Companies, i was use Laravel for Fullstack, Complex, and Enterprise App.",
     image: "/CM3.webp",
-    technologies: ["MySQL", "Laravel", "Tailwindcss", "Alpinejs", "Livewire"],
-    github: "#",
-    demo: "#",
+    technologies: ["MySQL", "Laravel", "PHP", "Tailwindcss", "Alpinejs", "Livewire"],
   },
   {
     id: 4,
@@ -36,27 +30,21 @@ export const projects = [
       "This is a My second Nextjs Project for Client, to many function re-rendered is make UX Slow, so i Use Nextjs for State Management.",
     image: "/flashcashier.webp",
     technologies: ["MongoDB", "Prisma", "Nextjs", "React", "Typescript", "Tailwindcss"],
-    github: "#",
-    demo: "#",
   },
   {
     id: 5,
-    title: "Ramen Delight",
+    title: "Pymoney",
     description:
-      "This is my first Web App use PHP Native dan Bootstrap for my first CSS Framework learning, Actually it's my School task Project.",
-    image: "/Ramen.webp",
-    technologies: ["MySQL", "PHP", "Javascript", "CSS", "Bootstrap",],
-    github: "#",
-    demo: "#",
+      "This is my first CRUD Project use Machine learning programming language, it's Python with his hundred Libraries",
+    image: "/pymoney.webp",
+    technologies: ["MySQL", "Python"],
   },
   {
     id: 6,
     title: "Artilia Companies",
     description:
-      "Artilia, This project contains company profiles from my team only without special features, the aim of creating this website is to introduce Artilia to everyone",
+      "Artilia, This project contains company profiles from my team only, cause this web just focused on UX & Performane so i use Nextjs for SSR, SEO Friendly",
     image: "/ArtiliaAI.webp",
     technologies: ["Nextjs", "React", "Javascript", "Tailwindcss"],
-    github: "#",
-    demo: "#",
   },
 ]

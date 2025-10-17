f0502da5494d318522f83c481ad553b07ab09905
setsuwww <setsuprojectonly@gmail.com>
2025-10-17 17:03:55 +0700

    tebelin garis grid

--- src/components/WelcomeBadge.jsx
+++ src/components/WelcomeBadge.jsx
@@ -1,6 +1,6 @@
 export default function WelcomeBadge() {
   return (
-    <div className="inline-flex items-center gap-2 px-4 py-2 rounded-full bg-gradient-to-l from-transparent to-green-500/10 glass-effect mb-8 mt-18 lg:mt-8 inset-shadow-xs inset-shadow-green-500/10">
+    <div className="inline-flex items-center gap-2 px-4 py-2 rounded-full border border-green-500/20 bg-gradient-to-l from-transparent to-green-500/10 glass-effect mb-8 mt-18 lg:mt-8">
       <div className="relative w-3 h-3" data-aos="zoom-in">
         <div className="absolute inset-0 rounded-full bg-green-400 opacity-75 animate-ping" />
         <div className="relative w-full h-full rounded-full bg-green-400 animate-pulse" />

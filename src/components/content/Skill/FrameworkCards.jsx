4e43c679a7c6d4ba390d5a014d14ed81bdc3bc71
setsuwww <setsuprojectonly@gmail.com>
2025-12-07 19:28:42 +0700

    UI update

--- src/components/content/Skill/FrameworkCards.jsx
+++ src/components/content/Skill/FrameworkCards.jsx
@@ -4,7 +4,7 @@ import { frameworks } from "../../../constants/frameworks"
 const FrameworkGrid = () => {
   const renderedFrameworks = useMemo(
     () => frameworks.map(({ Icon, title, color, bg, border, description }) => (
-          <div key={title} className={`group glass-effect relative rounded-2xl p-6 flex flex-col items-start hover:bg-zinc-500/10 border-b-0 border-l-2 hover:border-l-4 ${border} transition-all duration-100 ease-in`}>
+          <div key={title} className={`group glass-effect relative rounded-2xl p-6 flex flex-col items-start hover:bg-zinc-500/10 border-b-0 border-l-2 hover:border-l-[3px] ${border} transition-all duration-100 ease-in`}>
             <div className={`w-12 h-12 mb-4 rounded-full ${bg} flex items-center justify-center`}>
               <Icon className={`group-hover:stroke-[0.1px] w-8 h-8 ${color}`} />
             </div>
--- src/constants/frameworks.js
+++ src/constants/frameworks.js
@@ -5,7 +5,7 @@ export const frameworks = [
     Icon: SiNextdotjs,
     title: 'Nextjs',
     color: 'text-white',
-    bg: 'bg-gradient-to-br from-white/10 to-white/5 border-l-2 border-white/70',
+    bg: 'bg-gradient-to-br from-white/10 to-white/5 border-l-2 border-white/50',
     border: 'hover:bg-gradient-to-br hover:from-white/5 hover:to-transparent border-white/20 hover:border-white/40',
     description:
       'A powerful React framework with CSR, SSR, ISR, SSG, API Routing and SEO Performance',
@@ -14,7 +14,7 @@ export const frameworks = [
     Icon: SiNuxtdotjs,
     title: 'Nuxtjs',
     color: 'text-emerald-300',
-    bg: 'bg-gradient-to-br from-emerald-500/10 to-emerald-700/10 border-l-2 border-emerald-500',
+    bg: 'bg-gradient-to-br from-emerald-500/10 to-emerald-700/10 border-l-2 border-emerald-700',
     border: 'hover:bg-gradient-to-br hover:from-emerald-500/5 hover:to-transparent border-emerald-500/20 hover:border-emerald-500/40',
     description:
       'Vue-based framework with SSR and SSG same as Nextjs but Vue-based.',
